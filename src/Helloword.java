c4b6c08727959079a82f72775707e3b9bb83af39
wbq <wangbaoqun4585@163.com>
2017-03-14 19:08:47 +0800

    first commit

---
+++ src/Helloword.java
@@ -0,0 +1,12 @@
+import sun.applet.Main;
+
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+public class Helloword {
+
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+
+}
